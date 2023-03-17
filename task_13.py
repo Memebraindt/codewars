217ecff4a68f65bee986fc8762da765d3cda057f
MemeBraindt <IlyaBraindt@yandex.by>
2023-03-17 10:33:19 +0300

    upd t13-14

---
+++ task_13.py
@@ -0,0 +1,26 @@
+def solution(a):
+    mx = 0
+    mn = a[0]
+    for i in range(len(a)):
+        if a[i] > mx:
+            mx = a[i]
+        if a[i] < mn:
+            mn = a[i]
+    condition = False
+    while not condition:
+        flag = True
+        for i in range(len(a)):
+            if a[i] % mn == 0:
+                a[i] = mn
+            else:
+                a[i] %= mn
+                if a[i] < mn:
+                    mn = a[i]
+                flag = False
+        condition = flag
+    return mn*len(a)
+
+
+print(solution([9]))
+print(solution([6, 9, 21]))  # 9
+print(solution([1, 21, 55]))  # 3
--- task_14.py
+++ task_14.py
@@ -1,26 +1,20 @@
-def solution(a):
-    mx = 0
-    mn = a[0]
-    for i in range(len(a)):
-        if a[i] > mx:
-            mx = a[i]
-        if a[i] < mn:
-            mn = a[i]
-    condition = False
-    while not condition:
-        flag = True
-        for i in range(len(a)):
-            if a[i] % mn == 0:
-                a[i] = mn
-            else:
-                a[i] %= mn
-                if a[i] < mn:
-                    mn = a[i]
-                flag = False
-        condition = flag
-    return mn*len(a)
+lst = [0, 1, 1, ]
 
 
-print(solution([9]))
-print(solution([6, 9, 21]))  # 9
-print(solution([1, 21, 55]))  # 3
+def fib(n):
+    last = len(lst)
+    if last > n:
+        return lst[n]
+    else:
+        for i in range(last, n+1):
+            lst.append(lst[i-1] + lst[i-2])
+        last = n
+        return lst[n]
+
+
+print(fib(5))
+# print("lst = ", lst)
+print(fib(100))
+# print("lst = ", lst)
+print(fib(1000))
+# print("lst = ", lst)
