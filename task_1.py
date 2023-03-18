37dbd8904787bf573b2b17bc4730ef7b79c073be
MemeBraindt <IlyaBraindt@yandex.by>
2023-03-18 10:38:49 +0300

    added links for tasks

--- task_1.py
+++ task_1.py
@@ -1,5 +1,12 @@
+"""
+https://www.codewars.com/kata/514b92a657cdc65150000006
+Multiples of 3 or 5
+"""
+
+
 def solution(number):
     return sum([i if (i % 3 == 0) or (i % 5 == 0) else 0 for i in range(number)])
+
 
 print(solution(4))
 print(solution(-1))
--- task_14.py
+++ task_14.py
@@ -1,20 +1,44 @@
-lst = [0, 1, 1, ]
-
-
+"""
+https://www.codewars.com/kata/53d40c1e2f13e331fc000c26
+The Millionth Fibonacci Kata
+"""
+import decimal
 def fib(n):
-    last = len(lst)
-    if last > n:
-        return lst[n]
-    else:
-        for i in range(last, n+1):
-            lst.append(lst[i-1] + lst[i-2])
-        last = n
-        return lst[n]
-
+    decimal.getcontext().prec = 100000
+    root_5 = decimal.Decimal(5).sqrt()
+    phi = ((1 + root_5) / 2)
+    a = ((phi ** n) - ((-phi) ** -n)) / root_5
+    return round(a)
 
 print(fib(5))
-# print("lst = ", lst)
-print(fib(100))
-# print("lst = ", lst)
+print(fib(1))
+print(fib(-2))
+print(fib(5))
+print(fib(-6))
 print(fib(1000))
-# print("lst = ", lst)
+print(fib(-1000))
+
+
+
+# left_num = 0
+# left = 0
+# mid = 1
+# mid_num = 1
+# right = 2
+# right_num = 1
+#
+# def count_fib(n, l, m, r, lz, mz, rz):
+#     res = 0
+#     cur
+#     if n > r:
+#         for x in range(r, n+1):
+#
+#     return res
+#
+# def fib(n):
+#     return count_fib(n, left, mid, right, left_num, mid_num, right_num)
+#
+#
+# print(fib(5))
+# print(fib(100))
+# print(fib(1000))
