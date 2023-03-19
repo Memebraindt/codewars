d3b160034fffe05e08056d7db83725ff1681a28f
MemeBraindt <IlyaBraindt@yandex.by>
2023-03-19 22:15:40 +0300

    added links for tasks

--- task_2.py
+++ task_2.py
@@ -1,6 +1,13 @@
+"""
+https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+Complementary DNA
+"""
+
+
 def DNA_strand(dna):
     s1 = "ACTG"
     return "".join([s1[(s1.find(dna[i])+2) % 4] for i in range(len(dna))])
+
 
 print(DNA_strand("ATCG"))
 print(DNA_strand("ATTGC"))
